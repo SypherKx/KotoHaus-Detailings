0ed31efc9998b239ff944d09bc56e0026886e35b
SypherKx <itskaranpratapsingh@gmail.com>
2026-04-19 20:58:38 +0530

    style: bumped logo size up another 50 percent

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
           <img 
             src={kotohausLogo} 
             alt="Kotohaus Detailing Logo" 
-            className="h-[85px] md:h-[110px] w-auto mix-blend-screen transition-all duration-300 hover:opacity-80" 
+            className="h-[127px] md:h-[165px] w-auto mix-blend-screen transition-all duration-300 hover:opacity-80" 
           />
         </Link>
 
