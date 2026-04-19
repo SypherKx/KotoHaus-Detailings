70b746f488a5cb34fe888e25ed632a56220047f7
SypherKx <itskaranpratapsingh@gmail.com>
2026-04-19 21:22:11 +0530

    style: matched layout alignment to reference image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
           <img 
             src={kotohausLogo} 
             alt="Kotohaus Detailing Logo" 
-            className="h-[110px] md:h-[140px] w-auto mix-blend-screen transition-all duration-300 hover:opacity-80" 
+            className="h-10 md:h-12 w-auto mix-blend-screen transition-all duration-300 hover:opacity-80" 
           />
         </Link>
 
